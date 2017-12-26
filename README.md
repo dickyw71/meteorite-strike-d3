8d33af67bef4ab343a1a8f8c7224b92e7de9ba2e
Richard Wheatley <dickyw71@users.noreply.github.com>
2017-12-26 19:17:22 +0000

    Initial commit.

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # meteorite-strike-d3
+
+define map area
+
+Load world map data from http://bl.ocks.org/mbostock/raw/4090846/world-110m.json
+
+set the projection ?
+
+Load the meteorite stike data and combine with to the map data
+
+CLI
+
---
+++ js/index.js
@@ -0,0 +1,33 @@
+var width = 1100;
+var height = 960;
+
+var projection = d3.geoNaturalEarth1();
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+var path = d3.geoPath(projection);
+
+var g = svg.append("g");
+
+var radius = d3.scaleSqrt()
+    .domain([0, 1e6])
+    .range([0, 10]);
+
+d3.json("strikes-map.json", function(error, topology) {
+
+    g.selectAll(".map")
+    .data(topojson.feature(topology, topology.objects.world).features)
+    .enter()
+      .append("path")
+      .attr("class", "map")
+      .attr("d", path);
+
+    g.selectAll(".strike")
+    .data(topology.objects.strikes.geometries)
+    .enter()
+        .append("path")
+        .attr("class", "strike")
+        .attr("d", path.pointRadius((d) => radius(d.properties.mass)));
+});
